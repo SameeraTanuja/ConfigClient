3fdbf05db312995a8d3bafd706847ba2cfc9b81e
lpilaka <lpilaka@IN-MUM-LDVM022.corp.capgemini.com>
2018-10-12 09:08:32 -0400

    commit

---
+++ src/main/java/com/capgemini/configclient/MessageResource.java
@@ -0,0 +1,23 @@
+package com.capgemini.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageResource {
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@Value("${message:Default Hello}")
+	private String message;
+	@GetMapping("/message")
+	public String getMessage() {
+		return message;
+	}
+
+}
